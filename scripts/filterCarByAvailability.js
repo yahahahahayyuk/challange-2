5073a876090c3cd45f8a05c45e4668dc49251fa5
farid <shidqiahmad123@gmail.com>
2023-03-30 02:51:33 +0700

    challange 2 final

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,17 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+  cars.forEach(element => {
+    if(element.available == true) {
+      result.push(element);
+    }
+  });
+
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
